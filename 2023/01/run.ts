bb167eabadf58008d0ef8ce4d5e9cc7bae669fb0
Jani Haiko <haiko.jani@gmail.com>
2023-12-01 16:17:23 +0200

    Upload 2023 Day 1

--- 2023/01/run.ts
+++ 2023/01/run.ts
@@ -1,1 +1,39 @@
-console.log("Hello world!");
+console.time("Run time");
+
+import fs from "fs";
+
+const rows = fs.readFileSync("./01/input", "utf-8")
+    .split("\r\n")
+    .filter(Boolean);
+
+// Part 1
+const firstDigit = /\d/;
+const lastDigit = /.*(\d)(?!.*\d)/;
+console.info("Part 1: " + rows
+    .map(row => (firstDigit.exec(row) ?? ["error"])[0] + (lastDigit.exec(row) ?? ["error"])[1])
+    .map(Number)
+    .reduce((a, b) => a + b, 0)
+);
+
+// Part 2
+const firstDigitSpelled = /\d|one|two|three|four|five|six|seven|eight|nine/;
+const lastDigitSpelled = /.*(\d|one|two|three|four|five|six|seven|eight|nine)(?!.*(?:\d|one|two|three|four|five|six|seven|eight|nine))/;
+const replaceMap: Record<string, string> = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+};
+console.info("Part 2: " + rows
+    .map(row => [(row.match(firstDigitSpelled) ?? ["error"])[0], (row.match(lastDigitSpelled) ?? ["error"])[1]])
+    .map(arr => arr.map(s => replaceMap[s] ?? s).join(""))
+    .map(Number)
+    .reduce((a, b) => a + b, 0)
+);
+
+console.timeEnd("Run time"); // ~3ms
